e6e2c60d19f7eea72b9db6012bee99b849452691
GeronXXX <lityagin.ru@gmail.com>
2023-02-19 22:45:52 +0300

    DZ3_v4_passed

--- src/test/java/AutomationPracticeForm.java
+++ src/test/java/AutomationPracticeForm.java
@@ -3,26 +3,31 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class DZ3 {
+public class AutomationPracticeForm {
     @AfterAll
     static void after() {
-        Configuration.holdBrowserOpen=true;
+        Configuration.holdBrowserOpen = true;
     }
 
     @BeforeAll
     static void beforeALL() {
         Configuration.browserSize = "2560x1440";
-        Configuration.baseUrl = "https://demoqa.com";}
+        Configuration.baseUrl = "https://demoqa.com";
+    }
 
     @Test
-    void AutomationPracticeForm() {
+    void practiceForm() {
 
         open("/automation-practice-form");
+
+        File img = new File("src/test/resources/images/img.jpg");
 
         $("#firstName").setValue("Test");
         $("#lastName").setValue("Testov");
@@ -30,14 +35,14 @@ public class DZ3 {
         $(byText("Other")).click();
         $("#userNumber").setValue("89999999999");
         $("#dateOfBirthInput").click();
-        $("[class=react-datepicker__month-select]").click();
+        $("#uploadPicture").uploadFile(img);
+        $(".react-datepicker__month-select").click();
         $(byText("March")).click();
         $(byText("13")).click();
         $("#subjectsInput").setValue("Moscow");
         $(byText("Sports")).click();
         $(byText("Reading")).click();
         $(byText("Music")).click();
-        $("input[type=file]").sendKeys("E:/Users/ivant/Desktop/Безымянный.jpg");
         $("#currentAddress").setValue("Vetoshnyy Pereulok, 5, офис 219, Moscow, 109012");
         $(byText("Select State")).click();
         $(byText("Uttar Pradesh")).click();
@@ -45,19 +50,10 @@ public class DZ3 {
         $(byText("Lucknow")).click();
         $("#submit").click();
 
-        $("[class=table-responsive]").shouldHave(text("Test Testov"), text("Testovich@test.ru"), text("Other"),
-                text("8999999999"), text("13 March,2023"), text("Sports, Reading, Music"), text("Безымянный.jpg"),
-                text("Vetoshnyy Pereulok, 5, офис 219, Moscow, 109012"), text("Uttar Pradesh Lucknow"));
-
-
-
-
-
-
-
+        $("[class=table-responsive]").shouldHave(text("Test Testov"), text("Testovich@test.ru"), text("Other"), text("8999999999"), text("13 March,2023"), text("Sports, Reading, Music"), text("img.jpg"), text("Vetoshnyy Pereulok, 5, офис 219, Moscow, 109012"), text("Uttar Pradesh Lucknow"));
 
 
     }
-    }
+}
 
 
